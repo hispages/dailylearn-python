94c498dceca54b16ef8db2770a9e90832afa44d1
hispages <hisure.dev@gmail.com>
2025-07-23 13:59:13 +0800

    Adding explanatory comment to triangle pattern.py File

--- triangle pattern.py
+++ triangle pattern.py
@@ -1,5 +1,5 @@
-rows = int(input("rows: ")) 
-while rows <= 5 : 
+rows = int(input("rows: "))      #run and put number 1-5 here, it will continue add ing more star until 5 star or you can extend it
+while rows <= 5 :                # here you can extend it number so the star will continue to the number you pick
     collums = 1
     while collums <= rows :
         print("*", end=" ")
