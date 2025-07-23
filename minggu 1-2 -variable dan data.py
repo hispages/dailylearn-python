64808ea73fa6e7f0c5d4f61dd29fd84171450af1
hispages <hisure.dev@gmail.com>
2025-07-23 21:56:34 +0800

    chore: variable update

--- minggu 1-2 -variable dan data.py
+++ minggu 1-2 -variable dan data.py
@@ -1,6 +1,6 @@
 nama = "Khanda"        # Tipe data: string
 umur = 23             # Tipe data: integer
 tinggi = 177.8        # float
-kaya = True   # boolean
+mahasiswa = True   # boolean
 print("halo nama aku " + nama + " umur ku sudah " + str(umur) + " tinggi ku " + str(tinggi))
-print("rich ? " + str(kaya))   # begitu lah ya
+print("mahasiswa " + str(mahasiswa))   # begitu lah ya
