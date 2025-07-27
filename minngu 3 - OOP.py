5e0dcef71b42ee9163df9885a21f5e5d67cef004
hispages <hisure.dev@gmail.com>
2025-07-27 16:28:42 +0800

    feat: oop line of code

---
+++ minngu 3 - OOP.py
@@ -0,0 +1,12 @@
+class doll : 
+    def __init__(self,inside,outside,character,colors):
+        self.inside = inside
+        self.outside = outside
+        self.character = character
+        self.colors = colors
+
+    def sold(self):
+        print(f"{self.character} dolls with {self.colors} colored ar sold")
+
+    def sell(self):
+        print("f{self.character} dolls with {self.colored} are sell")
