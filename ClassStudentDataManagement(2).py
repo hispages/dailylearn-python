93b03463ef2533bc3a361289bba49cf8852ea339
hispages <hisure.dev@gmail.com>
2025-08-01 14:22:44 +0800

    feat: adding function to test class student data management

---
+++ ClassStudentDataManagement(2).py
@@ -0,0 +1,9 @@
+from ClassStudentDataManagement import student
+
+student1 = student("Lia", "21983481", 97)
+student2 = student("kadin", "47291643", 56)
+student3 = student("riski", "42184791", 44)
+
+student1.datalist()
+print("______________")
+student2.datalist()
