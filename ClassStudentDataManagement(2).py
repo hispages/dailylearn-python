296162390f65bc149f0438c2a9249531171cace3
hispages <hisure.dev@gmail.com>
2025-08-01 17:59:34 +0800

    feat: adding find, remove, update student scores, fixing uneffeciency code

--- ClassStudentDataManagement(2).py
+++ ClassStudentDataManagement(2).py
@@ -1,9 +1,47 @@
-from ClassStudentDataManagement import student
+from ClassStudentDataManagement import Student
+from ClassStudentDataManagement import Studentlist
 
-student1 = student("Lia", "21983481", 97)
-student2 = student("kadin", "47291643", 56)
-student3 = student("riski", "42184791", 44)
+#student1.datalist()
+#print("______________")
+#student2.datalist()
 
-student1.datalist()
-print("______________")
-student2.datalist()
+student1 = Student("Lia", "2191247", 97)
+student2 = Student("kadin", "3218312", 56)
+student3 = Student("riski", "4320847", 44)
+
+
+
+
+dataa = Studentlist()
+
+dataa.AddnewStudent(student1)
+dataa.AddnewStudent(student2)
+dataa.AddnewStudent(student3)
+
+# you can also can type this way for more efficiency
+dataa.AddnewStudent(Student("Citra", "11223344", 75))
+
+
+# to show all student you can run this " dataa.all_listOfstudent "
+
+
+# you can find student Name/id by filling the string with name or id \/
+# example.
+
+#find = dataa.find_student("")
+#if find:
+#    find.datalist()
+
+# here you can remove certain student by filling the string with name or id
+# example.
+
+#dataa.remove_student("")
+
+# here you can update student scores by filling the string and put new score after the comma (,)
+# example.
+
+#dataa.new_scores("", )
+
+
+
+dataa.all_listOfstudent()
--- ClassStudentDataManagement.py
+++ ClassStudentDataManagement.py
@@ -1,4 +1,4 @@
-class student:
+class Student:
     def __init__(self, name, student_id, scores):
         self.name = name
         self.student_id = student_id
@@ -8,32 +8,83 @@ class student:
         print(f"Name: {self.name}")
         print(f"Student ID: {self.student_id}")
         print(f"Score: {self.scores}")
+        print(f"grade: {self.calculategrade()}")
 
     def calculategrade(self):
-        if self.nilai >=85 :
+        if self.scores >=85 :
             return "A"
         
-        elif self.nilai >=70 :
+        elif self.scores >=70 :
             return "B"
         
-        elif self.nilai >=55 :
+        elif self.scores >=55 :
+            return "C"
+        
+        elif self.scores >=40 :
             return "D"
-        
-        elif self.nilai >=40 :
-            return "c"
-        
+
         else:
             return "E"
-        
+
 
 class Studentlist:
     def __init__(self):
-        self.list =[]
+        self.list = []
 
     def AddnewStudent(self, student):
         self.list.append(student)
     
     def all_listOfstudent(self):
-        for stundet in self.list:
+        for student in self.list:
             print("______________")
-            student.datalist
+            student.datalist()
+
+    def find_student(self, put):
+        for student in self.list:
+        
+            if student.name == put:
+                return student
+            elif student.student_id == put:
+                return student
+        return None
+    
+    #def find_id(self, student_id):
+        for student in self.list:
+        
+            if student.student_id == student_id:
+                return student
+        return None
+    
+    def new_scores(self, put, Score_update):
+        student = self.find_student(put)
+        if student:
+            student.scores = Score_update
+            return True
+        elif student:
+            student.scores = Score_update
+            return True
+        return False
+    
+    #def new_scores(self, name, Score_update):
+        student = self.find_id(name)
+        if student:
+            student.scores = Score_update
+            return True
+        return False
+    
+    def remove_student(self, Remove):
+        for student in self.list:
+            if student.student_id == Remove:
+                self.list.remove(student)
+                return True
+            elif student.name == Remove:
+                self.list.remove(student)
+                return True
+        return False
+        
+   # def remove_id(self, name):
+        for student in self.list:
+            if student.name == name:
+                self.list.remove(student)
+                return True
+        return False
