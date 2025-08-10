70ac6392f84bc382c1f9b42762775471d3b11e57
hispages <hisure.dev@gmail.com>
2025-08-10 17:52:07 +0800

    Feat: adding code to understand Open(), read() and Write()

---
+++ minggu3_OpenWriteRead.py
@@ -0,0 +1,21 @@
+with open("Write.txt", "w") as file:
+    file.write("first line.\n")
+    file.write("I'm write something here!\n")
+
+print("succesfully writed in Write.txt\n")
+
+with open("Write.txt", "r") as file:
+    print("result:")
+    r = file.read()
+    print(r)
+
+with open("Write.txt", "a") as file:
+    file.write("\nnew line.\n")
+    file.write("Great!")
+
+print("\nNew line has been succesfully added")
+
+with open("Write.txt", "r") as file:
+    print("result:")
+    nr = file.read()
+    print(nr)
